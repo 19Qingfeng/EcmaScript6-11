8b952a167a8b9ce903094810bcb4fef7d3af355e
wanghaoyu <wanghaoyu@zhuoyuan-info.com>
2020-08-24 14:10:29 +0800

    1Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # Es6-Es11
+
 EcmaScript6-11常用语法以及场景Demo。<br>
+
 ![ECMAScript2015_ES6_.png](https://i.loli.net/2020/08/14/IKn89JpoZX6YGbv.png)
+
 <br>
-1. EcmaScript6
-+ 函数
-+ 数组
-+ 对象
-+ 正则
-+ 字符串
-+ Symbol
-+ Set
-+ Map
-+ 解构赋值
-+ rest参数和扩展运算符
-+ 类与继承
-+ Proxy与Reflect
-+ Promise
-+ Generator
-+ Iterator
+
+## EcmaScript6
+
+* 块级作用域，let，const。(Demo1)
+    1. let
+        1. let 声明的全局变量不是全局对象window的属性
+        2. 同一作用域中用let定义变量不允许重复声明
+        3. let声明的变量不存在变量提升
+        4. let声明的变量具有暂时性死区
+        5. let 声明的变量拥有块级作用域
+    2. const
+        1. const定义的是常亮，不可修改。
+        2. const 实际上保证的并不是变量的值不得改动，而是变量指向的那个内存地址所保存的数据不得改动。
+
+* 解构赋值
+* 数组
+* 对象
+* 正则
+* 字符串
+* Symbol
+* Set
+* Map
+* 解构赋值
+* rest参数和扩展运算符
+* 类与继承
+* Proxy与Reflect
+* Promise
+* Generator
+* Iterator
--- src/demo/1.js
+++ src/demo/1.js
@@ -1,3 +1,95 @@
-const a = '2234dfsa4345'
+// 非window
+let a = 'let statement'
+console.log(window.a) // undefined
 
-console.log(a)
+
+// 不可重复声明
+let a = 5
+let a = 6
+// var可以重复进行覆盖 let在同一作用域中
+// VM131:1 Uncaught SyntaxError: Identifier 'a' has already been declared
+//   at <anonymous>:1:1
+
+// 不存在变量提升
+function foo() {
+    console.log(a) // var 变量提升 JS预加载
+    var a = 5
+}
+
+// 相当于
+// function foo() {
+//     var a
+//     console.log(a)
+//     a = 5
+// }
+
+foo() //undefined
+
+
+function foo() {
+    console.log(a)
+    let a = 5
+}
+
+foo()
+// Uncaught ReferenceError: Cannot access 'a' before initialization
+
+
+
+//  let声明的变量具有暂时性死区
+var a = 5
+if (true) {
+    a = 6
+    let a
+}
+// Uncaught ReferenceError: Cannot access 'a' before initialization
+// 先使用在声明相同作用域内会报错
+
+// 块级作用域
+{
+    let a = 3;
+}
+console.log(a) // undefined
+
+
+/* 常见面试题 let和const 闭包的区别 */
+for (var i = 0; i < 3; i++) {
+    console.log('循环内:' + i) // 0、1、2
+}
+console.log('循环外:' + i) // 3 
+
+for (let i = 0; i < 3; i++) {
+    setTimeout(function () {
+        console.log(i)
+    })
+}
+// 3、3、3
+// 想要得到1,2,3
+
+// 方法1：使用闭包 匿名函数自执行传递参数结合function作用域
+for (var i = 0; i < 3; i++) {
+    (function (j) {
+        setTimeout(function () {
+            console.log(j)
+        })
+    })(i)
+}
+
+// 方法2使用let
+for (let i = 0; i < 3; i++) {
+    setTimeout(function () {
+        console.log(i)
+    })
+}
+
+
+// const 定义常亮不可修改
+const a = 3;
+a = 4 // 报错
+
+const obj = {
+    a:"3"
+}
+obj["a"] = 6 
+obj["b"] = 3
+// 这是可以的，因为实际指向的内存地址并没有改变。
